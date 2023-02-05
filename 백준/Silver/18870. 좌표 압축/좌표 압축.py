1df8fc618f82a122a3c81cb9e8da51068cb0873a
fsd91 <121768905+fsd91@users.noreply.github.com>
2023-02-05 20:06:09 +0900

    [Silver II] Title: 좌표 압축, Time: 1884 ms, Memory: 154804 KB -BaekjoonHub

---
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -0,0 +1,9 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+n_list = list(map(int, input().split()))
+
+m_list = sorted(list(set(n_list)))
+dic = {m_list[i] : i for i in range(len(m_list))}
+for i in n_list:
+    print(dic[i], end = ' ')
