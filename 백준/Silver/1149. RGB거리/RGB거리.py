e2f25f1adcc06230dafa44818e9279d0b045c6af
dayhang <121768905+dayhang@users.noreply.github.com>
2023-02-25 07:20:34 +0900

    [Silver I] Title: RGB거리, Time: 80 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.py
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -1,14 +1,11 @@
-#1149
 n = int(input())
 h = []
+for _ in range(n):
+    h.append(list(map(int, input().split())))
 
-for i in range(n):
-  h.append(list(map(int,input().split())))
-for i in range(1, len(h)):
-  h[i][0] = min(h[i-1][1],h[i-1][2]) + h[i][0]
-  h[i][1] = min(h[i-1][0],h[i-1][2]) + h[i][1]
-  h[i][2] = min(h[i-1][0],h[i-1][1]) + h[i][2]
-hmin = min(h[n-1][0],h[n-1][1],h[n-1][2])
-print(hmin)
-
-
+for i in range(1,len(h)):
+    h[i][0] = min(h[i-1][1],h[i-1][2]) + h[i][0]
+    h[i][1] = min(h[i-1][0],h[i-1][2]) + h[i][1]
+    h[i][2] = min(h[i-1][0],h[i-1][1]) + h[i][2]
+    
+print(min(h[n-1]))
